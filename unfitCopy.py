40aca5d161fc61a6d22e7f84ac18c145108ce4fa
yhzyly <1489255957@qq.com>
2024-03-22 15:16:03 +0800

    修改unfitCopy只从点选文件夹读文件

--- unfitCopy.py
+++ unfitCopy.py
@@ -6,16 +6,18 @@ import shutil
 with open('invalid_images.txt', 'r' ,encoding="utf-8") as file:
     file_names = file.read().splitlines()
 
-# 检查是否有(1)后缀的图片并保存到另一个文件夹
-for file_name in file_names:
-    file_path = os.path.join("点选", file_name)  # 添加pic/前缀
-    if os.path.exists(file_path):
-        base_name, extension = os.path.splitext(file_path)
-        base_name_target, extension1 = os.path.splitext(file_name)
-        print(base_name, extension)
+strlist = ["点选","拖拽","填空"]
+for i in range(3):
+    for file_name in file_names:
+        file_path = os.path.join(strlist[i], file_name)  # 添加pic/前缀
+        if os.path.exists(file_path):
+            base_name, extension = os.path.splitext(file_path)
+            base_name_target, extension1 = os.path.splitext(file_name)
+            print(base_name, extension)
 
-        # 复制文件到另一个文件夹并以原文件名保存
-        shutil.copy(file_path, f'output/{base_name_target}{extension}')
-        print(f'复制文件 {file_name} 到 output 文件夹')
+            # 复制文件到另一个文件夹并以原文件名保存
+            shutil.copy(file_path, f'output/{base_name_target}{extension}')
+            print(f'复制文件 {file_name} 到 output 文件夹')
+    
 
 print('处理完成')
